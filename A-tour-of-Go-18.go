0dd72191da3be5165a2363eeb09bffb9065f055c
Jeon Ga Bin <hyanghope@naver.com>
2019-06-14 21:24:36 +0900

    Chapter 18 review

--- A-tour-of-Go-18.go
+++ A-tour-of-Go-18.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-    sum := 1
-    for sum < 1000 {
-        sum += sum
-    }
-    fmt.Println(sum)
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
 }
+
+// 복습 완료
