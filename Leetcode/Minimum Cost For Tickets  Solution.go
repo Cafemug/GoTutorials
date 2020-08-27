177e474d88a4333f68b3578efe7cd2a2b9d875a3
Jeon Ga Bin <hyanghope@naver.com>
2020-08-28 01:36:09 +0900

    dp로 구현

--- Leetcode/Minimum Cost For Tickets  Solution.go
+++ Leetcode/Minimum Cost For Tickets  Solution.go
@@ -1,43 +1,28 @@
-var check = make([]int, 366, 366)
-
 func min(a, b int) int {
 	if a < b {
 		return a
 	}
 	return b
 }
-
-func contain(days []int, day int) bool {
-	for _, item := range days {
-		if item == day {
-			return true
-		}
+func max(a, b int) int {
+	if a >= b {
+		return a
 	}
-	return false
-}
-
-func dp(i int, days []int, costs []int) int {
-	if i > 365 {
-		return 0
-	}
-	if check[i] != 0 {
-		return check[i]
-	}
-	var ans int
-	if contain(days, i) == true {
-		ans = min(min(dp(i+1, days, costs)+costs[0],
-			dp(i+7, days, costs)+costs[1]),
-			dp(i+30, days, costs)+costs[2])
-
-	} else {
-		ans = dp(i+1, days, costs)
-
-	}
-	check[i] = ans
-
-	return ans
+	return b
 }
 
 func mincostTickets(days []int, costs []int) int {
-	return dp(1, days, costs)
+	var dp = make([]int, 366, 366)
+	max_day := days[len(days)-1]
+	for d, i := 1, 0; d <= max_day; d++ {
+		if d == days[i] {
+			dp[d] = min(min(dp[d-1]+costs[0], dp[max(0, d-7)]+costs[1]), dp[max(0, d-30)]+costs[2])
+			i++
+
+		} else {
+			dp[d] = dp[d-1]
+		}
+
+	}
+	return dp[max_day]
 }
