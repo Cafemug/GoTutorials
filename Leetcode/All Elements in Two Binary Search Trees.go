4b01ddf5b61a1b8acea554e8b8c60053a53d0df5
Jeon Ga Bin <hyanghope@naver.com>
2020-09-06 15:09:18 +0900

    All Elements in Two Binary Search Tree 풀이

---
+++ Leetcode/All Elements in Two Binary Search Trees.go
@@ -0,0 +1,28 @@
+import "sort"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func makeList(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	result := append([]int{}, root.Val)
+	res1 := makeList(root.Left)
+	res2 := makeList(root.Right)
+	result = append(result, res1...)
+	result = append(result, res2...)
+	return result
+}
+func getAllElements(root1 *TreeNode, root2 *TreeNode) []int {
+	result_left := makeList(root1)
+	result_right := makeList(root2)
+	result := append(result_left, result_right...)
+	sort.Ints(result)
+	return result
+}
